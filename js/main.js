f8214acfe79151b28d6ab28b463c0605901af5a9
Jeong-eunbi <ag11041@naver.com>
2021-03-27 00:05:03 +0900

    공통모듈분리

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
 'use strict'// 자바스크립트는 보다 유연한언어이기때문에 다른 언어에서는 허용되지않는 비상적인일들을 방지하기위함. (선언되지않은 변수의값을할당한다던지.. )
 
-// 검색창제어
-const searchEl = document.querySelector('.search'); // 검색창 요소 찾기-document에서 클래스가search인 선택자를 찾아서 searchEl에 할당
-const searchInputEl = searchEl.querySelector('input');// 찾은 searchEl안에서 input을 찾아서 searchInputEl할당,더효율적으로 찾기위해 찾아놓은 요소안에서 찾게하기
-//검색창 요소를 클릭하면 실행
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-// 검색창 요소 input에 포커스가 됬을때 명령을 실행
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-// focused라는 클래스를 추가,focus가 됬을때 focused라는 클래스가 적용(css에서 어떤것을 할지 적용)
-  searchInputEl.setAttribute('placeholder', '통합검색'); //setAttribute- html에 어떤 속성을 지정할때 사용하는 메소드,(placeholder-힌트,힌트의값)
-});
-
-//검색창에서 input요소 포커스가 해제(블러)되면 실행
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused')
-  searchInputEl.setAttribute('placeholder', '')
-});
-
 // 페이지 스크롤 제어
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -200,6 +180,4 @@ new Swiper('.awards .swiper-container', {
   }
 });
 
-//올해몇년도인지 계산
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear()
+
